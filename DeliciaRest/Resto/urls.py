3fad34ec39d9e923b9c57504dad0d9689a62346b
Matias <101906720+Grizxz@users.noreply.github.com>
2023-05-24 14:07:21 -0400

    URL de registro Agregada

--- DeliciaRest/Resto/urls.py
+++ DeliciaRest/Resto/urls.py
@@ -5,9 +5,10 @@ from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
 
     path('',index,name=""),
-
+    #Configuración del Login
     path('login/',login, name="login"),
-    
     path('login',LoginView.as_view(template_name='registration/login.html'),name="login"),
     path('logout',LogoutView.as_view(template_name='app/index.html'),name="logout"),
+    #Configuración de Registro
+    path('register', register, name="agregarUsuario")
 ]
